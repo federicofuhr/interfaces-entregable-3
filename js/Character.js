55114f0a5c91edc75d21dd03f9d396afcc3d2511
Federico Fuhr <46513550+federicofuhr@users.noreply.github.com>
2021-10-29 23:28:12 -0300

    comentada clase personaje y moneda

--- js/Character.js
+++ js/Character.js
@@ -1,4 +1,7 @@
 export default class Character {
+    /**
+     * CONSTRUCTOR DE LA CLASE DEL PERSONAJE
+     */
     constructor() {
         this.character = document.querySelector("#character");
         this.topPosition = character.offsetTop;
@@ -9,26 +12,44 @@ export default class Character {
         this.dead = false;
     }
 
+    /**
+     * RETORNA LA POSICION EN X
+     */
     getPositionX() {
         return this.character.offsetLeft;
     }
 
+    /**
+     * RETORNA LA POSICION EN Y
+     */
     getPositionY() {
         return this.character.offsetTop;
     }
 
+    /**
+     * RETORNA EL ANCHO DEL PERSONAJE
+     */
     getWidth() {
         return this.width;
     }
 
+    /**
+     * RETORNA EL ALTO DEL PERSONAJE
+     */
     getHeight() {
         return this.height;
     }
 
+    /**
+     * RETORNA UN BOOLEANO SI EL PERSONAJE MURIO
+     */
     isDead() {
         return this.dead;
     }
 
+    /**
+     * EL ESTADO DEL PERSONAJE CAMBIA A MUERTO
+     */
     setDead() {
         this.dead = true;
     }
@@ -37,23 +58,36 @@ export default class Character {
         console.log(this);
     }
 
+    /**
+     * CLASE QUE TIENE EL JUGADOR PARA LA ANIMACION DE CORRER
+     */
+
     run() {
         this.character.classList.toggle(this.currentClassName);
         this.currentClassName = "character-running";
     }
 
+    /**
+     * CLASE QUE TIENE EL JUGADOR PARA LA ANIMACION DE SALTAR
+     */
     jump() {
         this.character.classList.toggle(this.currentClassName);
         this.currentClassName = "character-jump";
         this.character.classList.toggle(this.currentClassName);
     }
 
+    /**
+     * CLASE QUE TIENE EL JUGADOR PARA LA ANIMACION DE DESLIZARSE Y BAJAR RAPIDO
+     */
     slide() {
         this.character.classList.toggle(this.currentClassName);
         this.currentClassName = "character-slide";
         this.character.classList.toggle(this.currentClassName);
     }
 
+    /**
+     * CLASE QUE TIENE EL JUGADOR PARA LA ANIMACION DE MORIR
+     */
     die() {
         this.setDead();
         this.character.classList.toggle(this.currentClassName);
@@ -62,6 +96,9 @@ export default class Character {
         //this.character.classList.add()
     }
 
+    /**
+     * ANIMACION POR DEFECTO DEL PERSONAJE
+     */
     defaultAnimation() {
         if (this.currentClassName != "character-running") {
             this.character.classList.toggle(this.currentClassName);
@@ -70,6 +107,9 @@ export default class Character {
         }
     }
 
+    /**
+     * ASIGNACION DE BOTONES PARA LAS ACCIONES DEL PERSONAJE
+     */
     action(event) {
         const key = event.key;
         switch (key) {
--- js/Coin.js
+++ js/Coin.js
@@ -1,4 +1,7 @@
 export default class Coin {
+    /**
+     * CONTRUCTOR DE LA CLASE MONEDA
+     */
     constructor() {
         this.coin = document.querySelector("#coin");
         this.topPosition = this.coin.offsetTop;
@@ -8,34 +11,55 @@ export default class Coin {
         this.currentClassName = this.coin.className;
     }
 
+    /**
+     * CLASE QUE TIENE LA CLASE MONEDA PARA LA ANIMACION DE MOVERSE
+     */
     scroll() {
         this.coin.classList.toggle(this.currentClassName);
         this.currentClassName = "coin-scroll";
         this.coin.classList.toggle(this.currentClassName);
     }
 
+    /**
+     * CLASE DE LA CLASE MONEDA PARA LA ANIMACION DE GIRAR CUANDO LA AGARRAN
+     */
     spin() {
         this.coin.classList.toggle(this.currentClassName);
         this.currentClassName = "coin-spin";
         this.coin.classList.toggle(this.currentClassName);
     }
 
+    /**
+     * RETORNA LA MONEDA
+     */
     getDiv() {
         return this.coin;
     }
 
+    /**
+     * RETORNA LA POSICION EN EL EJE X DE LA MONEDA
+     */
     getPositionX() {
         return this.coin.offsetLeft;
     }
 
+    /**
+     * RETORNA LA POSICION EN EL EJE Y DE LA MONEDA
+     */
     getPositionY() {
         return this.coin.offsetTop;
     }
 
+    /**
+     * RETORNA EL ANCHO DE LA MONEDA
+     */
     getWidth() {
         return this.width;
     }
 
+    /**
+     * RETORNA EL ANCHO DE LA MONEDA
+     */
     getHeight() {
         return this.height;
     }
